a779bb272125e7269ffb9aafa36c33dcf1850d8f
NircoRoman <nircoroman@gmail.com>
2022-05-31 20:58:38 +0300

    Leson10

---
+++ src/main/java/Leson10/MyArrayDataException.java
@@ -0,0 +1,6 @@
+package Leson10;
+    class MyArrayDataException extends RuntimeException {
+        MyArrayDataException(String msg) {
+            super("Ошибка преобразования элемента массива в целое число.\n" + " " + msg);
+    }
+}
